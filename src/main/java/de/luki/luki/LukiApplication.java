08e53d7a96579af2edd18aaa9335c787aee0c46c
Luckose <byjuluckose@gmail.com>
2022-01-26 22:28:14 +0100

    Initial commit

---
+++ src/main/java/de/luki/luki/LukiApplication.java
@@ -0,0 +1,13 @@
+package de.luki.luki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LukiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/luki/luki/LukiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.luki.luki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
